0e03aa1f8b9f54aec345316ea6e97bf1d82eab85
BrianC <seahorse8789@gmail.com>
2019-03-29 20:26:19 -0700

    added icons and links to our social media on the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,16 +20,13 @@ class Home extends Component {
 							Civilization Players League (CPL) is a longstanding Civilization community aiming to deliver a fun and friendly, yet competitive environment for Civilization VI players. Our community hosts numerous games at all hours of the day with players from all over the world. We strive to give our players the best experience free of the common problems found in public games such as players randomly quitting or you getting disconnected from a multiplayer game.
 							</p>
 						<ul>
-							<li><a>facebook</a></li>
-							<li><a>discord</a></li>
-							<li><a>youtube</a></li>
-							<li><a>steam</a></li>
+							<li><a href='https://www.facebook.com/groups/civplayers/' target='_blank'><img className='link-icon' src='/pics/facebook_icon.png'></img></a></li>
+							<li><a href='https://discord.gg/ubwTNdd' target='_blank'><img className='link-icon' src='/pics/discord_icon.jpg'></img></a></li>
+							<li><a href='https://www.youtube.com/user/TheCanuckSoldier' target='_blank'><img className='link-icon' src='/pics/youtube_icon.png'></img></a></li>
+							<li><a href='https://steamcommunity.com/gid/103582791431089902' target='_blank'><img className='link-icon' src='/pics/Steam_icon.png'></img></a></li>
 						</ul>
 					</div>
 				</div>
-
-
-
 			</div>
 		)
 	}
