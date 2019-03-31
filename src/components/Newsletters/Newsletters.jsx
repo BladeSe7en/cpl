ffc522a726562a458eb44bad47a6eabb9cc44a0b
BrianC <seahorse8789@gmail.com>
2019-03-31 03:26:09 -0700

    uninstalled express

--- src/components/Newsletters/Newsletters.jsx
+++ src/components/Newsletters/Newsletters.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router';
-import moment from 'moment';
 import Navbar from '../Navbar/Navbar';
 
 class Newsletters extends Component {
