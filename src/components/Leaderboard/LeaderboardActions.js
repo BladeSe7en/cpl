01354b0c5f15e7eb4e14277bfaa900983c13493a
BrianC <seahorse8789@gmail.com>
2019-04-06 05:30:19 -0700

    finally fixed the bugs related to the search by best civ. I decided to add  [gameType]AllLeader properties to the player model and then use loopback to filter the data. Now all that I need to do is when reporting games ill need to add the civ to the All leader array then filter that array to see ifthe incoming leader made the BestLeader change

--- src/components/Leaderboard/LeaderboardActions.js
+++ src/components/Leaderboard/LeaderboardActions.js
@@ -49,15 +49,16 @@ export const getData = (sortBy, sortOrder) => {
 	}
 }
 
-export const searchBy = (value, typeOfGame, BestCiv) => {
+export const searchBy = (selected, typeOfGame, BestLeader) => {
 	const accessToken = 'ZHWTGJ9sPahvLeQ8M97jtO7XwSlRGYJ4XZSMhprA8GzGNM5AXNcdJDtM67MHEIPx'
-	console.log('value in searchBy: ',value)
-	console.log('this is url: ',`http://localhost:3000/api/players?access_token=${accessToken}&filter[where][${typeOfGame}${BestCiv}]=${value}`)
+	console.log('selected in searchBy: ',selected)
+	console.log('this is url: ',`http://localhost:3000/api/players?access_token=${accessToken}&filter[where][${typeOfGame}${BestLeader}]=${selected}`)
 	return {
 		type: 'GET_DATA',
 		payload: Axios({
 			method: 'get',
-			url: `http://localhost:3000/api/players?access_token=${accessToken}&filter[where][${typeOfGame}${BestCiv}]=${value}`
+			url: `http://localhost:3000/api/players?filter=%7B%22where%22%3A%20%7B%22${typeOfGame}${BestLeader}%22%3A%20%22${selected}%22%7D%7D&access_token=${accessToken}`
+	      //url: `http://localhost:3000/api/players?filter=%7B%22where%22%3A%20%7B%22careerBestLeader%22%3A%20%22ZuluShaka%22%7D%7D&access_token=ZHWTGJ9sPahvLeQ8M97jtO7XwSlRGYJ4XZSMhprA8GzGNM5AXNcdJDtM67MHEIPx`
 		})
 			.then(response => {
 				return response.data
