f72375ff1acded4bf868de263ae301f90c3eb67a
seed.of.apricot <42898720+keita-makino@users.noreply.github.com>
2019-04-09 01:01:35 -0700

    Update getPlayerStats.js

--- server/utils/getPlayerStats.js
+++ server/utils/getPlayerStats.js
@@ -1,77 +1,80 @@
-const app = require('../server')
+const app = require('../server');
 
 function getPlayerStats(names) {
-    console.log('names: ', names)
-    return new Promise((resolve, reject) => {
-        const { Player } = app.models;
-        const playerData = names.map(name => {
-
-            console.log('single name: ', name)
-            const defaultSkill = 1500;
-            Player.findOrCreate({ where: { name: name } },
-                {
-                    "name": name,
-                    "careerSkill": defaultSkill,
-                    "careerHighestSkill": defaultSkill,
-                    "careerWins": 0,
-                    "careerLosses": 0,
-                    "careerPercent": 0,
-                    "careerBestLeader": "none",
-                    "careerAllLeader": ["none"],
-                    "highestCareerSkill": defaultSkill,
-                    "careerSeasonWins": 0,
-                    "careerSeasonLosses": 0,
-                    "careerSeasonPercent": 0,
-                    "teamSkill": defaultSkill,
-                    "highestTeamSkill": defaultSkill,
-                    "teamWins": 0,
-                    "teamLosses": 0,
-                    "teamPercent": 0,
-                    "teamBestLeader": "none",
-                    "teamAllLeader": ["none"],
-                    "teamSeasonWins": 0,
-                    "teamSeasonLosses": 0,
-                    "teamSeasonPercent": 0,
-                    "duelSkill": defaultSkill,
-                    "highestDuelSkill": defaultSkill,
-                    "duelWins": 0,
-                    "duelLosses": 0,
-                    "duelPercent": 0,
-                    "duelBestLeader": "none",
-                    "duelAllLeader": ["none"],
-                    "duelSeasonWins": 0,
-                    "duelSeasonLosses": 0,
-                    "duelSeasonPercent": 0,
-                    "ffaSkill": defaultSkill,
-                    "highestffaSkill": defaultSkill,
-                    "ffaWins": defaultSkill,
-                    "ffaLosses": 0,
-                    "ffaPercent": 0,
-                    "ffaBestLeader": "none",
-                    "ffaAllLeader": ["none"],
-                    "ffaSeasonWins": 0,
-                    "ffaSeasonLosses": 0,
-                    "ffaSeasonPercent": 0,
-                    "careerHighSkill": defaultSkill,
-                    "teamHighSkill": defaultSkill,
-                    "duelHighSkill": defaultSkill,
-                    "ffaHighSkill": defaultSkill
-                },
-                (err) => {
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
-                }
-            )
+  console.log('names: ', names);
+  return new Promise((resolve, reject) => {
+    const { Player } = app.models;
+    const playerData = names.map(name => {
+      console.log('single name: ', name);
+      const defaultSkill = 1500;
+      Player.findOrCreate(
+        { where: { name: name } },
+        {
+          name: name,
+          careerSkill: defaultSkill,
+          careerHighestSkill: defaultSkill,
+          careerWins: 0,
+          careerLosses: 0,
+          careerPercent: 0,
+          careerBestLeader: 'none',
+          careerAllLeader: ['none'],
+          highestCareerSkill: defaultSkill,
+          careerSeasonWins: 0,
+          careerSeasonLosses: 0,
+          careerSeasonPercent: 0,
+          teamSkill: defaultSkill,
+          highestTeamSkill: defaultSkill,
+          teamWins: 0,
+          teamLosses: 0,
+          teamPercent: 0,
+          teamBestLeader: 'none',
+          teamAllLeader: ['none'],
+          teamSeasonWins: 0,
+          teamSeasonLosses: 0,
+          teamSeasonPercent: 0,
+          duelSkill: defaultSkill,
+          highestDuelSkill: defaultSkill,
+          duelWins: 0,
+          duelLosses: 0,
+          duelPercent: 0,
+          duelBestLeader: 'none',
+          duelAllLeader: ['none'],
+          duelSeasonWins: 0,
+          duelSeasonLosses: 0,
+          duelSeasonPercent: 0,
+          ffaSkill: defaultSkill,
+          highestffaSkill: defaultSkill,
+          ffaWins: defaultSkill,
+          ffaLosses: 0,
+          ffaPercent: 0,
+          ffaBestLeader: 'none',
+          ffaAllLeader: ['none'],
+          ffaSeasonWins: 0,
+          ffaSeasonLosses: 0,
+          ffaSeasonPercent: 0,
+          careerHighSkill: defaultSkill,
+          teamHighSkill: defaultSkill,
+          duelHighSkill: defaultSkill,
+          ffaHighSkill: defaultSkill
+        }
+      )
+        .then(([data, isCreated]) => {
+          console.log(resolve(data));
+          resolve(data);
         })
-        Promise.all(playerData)
-            .then(results => resolve(results))
-            .catch(err => reject(new Error('could not return results')))
-    })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+    // Promise.all(playerData)
+    //   .then(results => {
+    //     console.log(resolve(results));
+    //     resolve(results);
+    //   })
+    //   .catch(err => reject(new Error('could not return results')));
+  });
 }
 
 module.exports = { getPlayerStats };
 
 //["player0", "player1", "player2"]
-
