90bb422d44a7f99520bb4c5fc6c34a1e53931671
BrianC <seahorse8789@gmail.com>
2019-04-14 20:03:57 -0700

    comitting to do a code review with a peer

--- src/components/Newsletters/Newsletters.jsx
+++ src/components/Newsletters/Newsletters.jsx
@@ -33,21 +33,45 @@ class Newsletters extends Component {
             
             console.log('allPlayerStats[0][0].name: ',allPlayerStats[0][0].name)   //this will return player0
             console.log('allPlayerStats[0][0].ffaSkill: ',allPlayerStats[0][0].ffaSkill) //this will return 1980
-         
+            const createVariable = () => {
+              const nameList = [];
+              allPlayerStats.map(playerName => {
+              return  nameList.push(playerName[0].name)
+              })
+              makeThePlayers(nameList)
+            }
+            createVariable()
 
-            allPlayerStats.map(playerName => (
-              [playerName[0].name] = ranking.makePlayer(playerName[0].ffaSkill, playerName[0].ffaSkill, playerName[0].ffaRatingDiviation, playerName[0].ffaVolatility)
-            ));
-
-            var matches = [[player0, player1, 1], [player0, player2, 1], [player1, player2, 1]];
-
+            function makeThePlayers(nameList) {
+         var madePlayers =[]
+              allPlayerStats.map((playerName, i) => {
+              console.log('i: ',i) 
+              console.log('nameList[i]: ',nameList[i])
+           var namme ={[nameList[i]]: ranking.makePlayer(playerName[0].ffaSkill, playerName[0].ffaRatingDiviation, playerName[0].ffaVolatility)}
+           madePlayers.push(namme)
+          }); 
+          evaluate(madePlayers)
+        }
+        function evaluate(madePlayers) {
+          console.log('madePlayers', madePlayers)
+          console.log('player0---: ',madePlayers[0].player0),
+          console.log('player1---: ',madePlayers[1].player1),
+          console.log('player2---: ',madePlayers[2].player2)
+          
+          const matches = [
+            [madePlayers[0].player0, madePlayers[1].player1, 1],
+            [madePlayers[0].player0, madePlayers[2].player1, 1],
+            [madePlayers[1].player1, madePlayers[2].player2, 1]];
+            
             ranking.updateRatings(matches)
+            
+           return madePlayers.map(i => (
+              console.log(`${madePlayers[i]} new rating: ` + madePlayers[i].getRating()),
+              console.log(`${madePlayers[i]} new rating deviation: ` + madePlayers[i].getRd()),
+              console.log(`${madePlayers[i]} new volatility: ` + madePlayers[i].getVol())
+              ));
+            }
 
-            allPlayerStats.map(playerName => (
-              console.log(`${playerName} new rating: ` + playerName.getRating()),
-              console.log(`${playerName} new rating deviation: ` + playerName.getRd()),
-              console.log(`${playerName} new volatility: ` + playerName.getVol())
-            ));
 
 
 
@@ -56,7 +80,40 @@ class Newsletters extends Component {
                  // updatedStats[volatility] = player.name.getVol();
               }
               
-          
+
+             // everything between here and and the render is another attempt to do what i am trying to do in the createVariable(), makeThePlayers(), and evaluate()
+
+
+
+          // heres what I have so far. What I want to happen is have playerName[0].name 
+// be the variable name and set that equal to the rest of the equation. That
+// way I can plug in the variable player0 into the matches array to run my 
+// calculations. The problem I am having is when the code is getting to the 
+// const matches array it is saying that player0 is undefined.
+// heres what I do know: 
+// allPlayerStats is a properly formatted json object.
+// I have console logged every bracket/dot notation property separately to know 
+// that all of them are written in proper syntax. 
+ //allPlayerStats.map(playerName => (
+  //[playerName[0].name] = ranking.makePlayer(playerName[0].ffaSkill, playerName[0].ffaRatingDiviation, playerName[0].ffaVolatility)
+  //));
+  // the expected output of the above map would look like this and all of // that data comes from the allPlayerStats object:
+  // var player0 = ranking.makePlayer(1989, 200, 0.06);
+  // var player1 = ranking.makePlayer(1500, 200, 0.06);
+  // var player2 = ranking.makePlayer(1750, 300, 0.06);
+  // then each one of those variables would be listed as shown below in the matches array
+  //  const matches = [
+  // [player0, player1, 1], 
+  // [player0, player2, 1], 
+  // [player1, player2, 1]];
+  
+  // ranking.updateRatings(matches)
+  
+  // allPlayerStats.map(playerName => (
+  // console.log(`${playerName[0].name} new rating: ` + playerName[0].name.getRating()),
+  // console.log(`${playerName[0].name} new rating deviation: ` + playerName[0].name.getRd()),
+  // console.log(`${playerName[0].name} new volatility: ` + playerName[0].name.getVol())
+  // ));
             }
   render() {
       return (
@@ -73,3 +130,190 @@ class Newsletters extends Component {
 }
 
 export default Newsletters;
+
+
+
+
+// [
+//   [
+//     {
+//       "name": "player0",
+//       "careerSkill": 2080,
+//       "careerHighestSkill": 1500,
+//       "careerWins": 260,
+//       "careerLosses": 100,
+//       "careerPercent": 72,
+//       "careerBestLeader": "ZuluShaka",
+//       "careerAllLeader": [
+//         "ZuluShaka"
+//       ],
+//       "id": 3,
+//       "highestCareerSkill": 1770,
+//       "careerSeasonWins": 210,
+//       "careerSeasonLosses": 21,
+//       "careerSeasonPercent": 91,
+//       "teamSkill": 1780,
+//       "highestTeamSkill": 1500,
+//       "teamWins": 210,
+//       "teamLosses": 10,
+//       "teamPercent": 95,
+//       "teamBestLeader": "IndiaGandhi",
+//       "teamAllLeader": [
+//         "GermanyFrederickBarbarossa"
+//       ],
+//       "teamSeasonWins": 20,
+//       "teamSeasonLosses": 2,
+//       "teamSeasonPercent": 91,
+//       "duelSkill": 1780,
+//       "highestDuelSkill": 1500,
+//       "duelWins": 10,
+//       "duelLosses": 45,
+//       "duelPercent": 72,
+//       "duelBestLeader": "HungaryMatthias",
+//       "duelAllLeader": [
+//         "ZuluShaka"
+//       ],
+//       "duelSeasonWins": 210,
+//       "duelSeasonLosses": 21,
+//       "duelSeasonPercent": 91,
+//       "ffaSkill": 1980,
+//       "highestffaSkill": 1500,
+//       "ffaWins": 230,
+//       "ffaLosses": 109,
+//       "ffaPercent": 67,
+//       "ffaBestLeader": "MapucheLautaro",
+//       "ffaAllLeader": [
+//         "ZuluShaka"
+//       ],
+//       "ffaSeasonWins": 25,
+//       "ffaSeasonLosses": 54,
+//       "ffaSeasonPercent": 72,
+//       "careerHighSkill": 2180,
+//       "teamHighSkill": 1780,
+//       "duelHighSkill": 1880,
+//       "ffaHighSkill": 1980
+//     },
+//     false
+//   ],
+//     [
+//       {
+//         "name": "player1",
+//         "careerSkill": 2080,
+//         "careerHighestSkill": 1500,
+//         "careerWins": 78,
+//         "careerLosses": 80,
+//         "careerPercent": 49,
+//         "careerBestLeader": "SwedenKristina",
+//         "careerAllLeader": [
+//           "ZuluShaka",
+//           "GermanyFrederickBarbarossa"
+//         ],
+//         "id": 4,
+//         "highestCareerSkill": 1330,
+//         "careerSeasonWins": 40,
+//         "careerSeasonLosses": 28,
+//         "careerSeasonPercent": 59,
+//         "teamSkill": 1990,
+//         "highestTeamSkill": 1500,
+//         "teamWins": 65,
+//         "teamLosses": 87,
+//         "teamPercent": 42,
+//         "teamBestLeader": "KoreaSeondeok",
+//         "teamAllLeader": [
+//           "GreeceGorgo"
+//         ],
+//         "teamSeasonWins": 54,
+//         "teamSeasonLosses": 43,
+//         "teamSeasonPercent": 56,
+//         "duelSkill": 1300,
+//         "highestDuelSkill": 1500,
+//         "duelWins": 21,
+//         "duelLosses": 11,
+//         "duelPercent": 63,
+//         "duelBestLeader": "AztecMontezuma",
+//         "duelAllLeader": [
+//           "GreeceGorgo"
+//         ],
+//         "duelSeasonWins": 34,
+//         "duelSeasonLosses": 76,
+//         "duelSeasonPercent": 31,
+//         "ffaSkill": 1770,
+//         "highestffaSkill": 1500,
+//         "ffaWins": 90,
+//         "ffaLosses": 23,
+//         "ffaPercent": 80,
+//         "ffaBestLeader": "BrazilPedroII",
+//         "ffaAllLeader": [
+//           "GreeceGorgo"
+//         ],
+//         "ffaSeasonWins": 22,
+//         "ffaSeasonLosses": 15,
+//         "ffaSeasonPercent": 60,
+//         "careerHighSkill": 2080,
+//         "teamHighSkill": 1990,
+//         "duelHighSkill": 1300,
+//         "ffaHighSkill": 1780
+//       },
+//       false
+//     ],
+//     [
+//       {
+//         "name": "player2",
+//         "careerSkill": 1680,
+//         "careerHighestSkill": 1500,
+//         "careerWins": 84,
+//         "careerLosses": 32,
+//         "careerPercent": 72,
+//         "careerBestLeader": "GreeceGorgo",
+//         "careerAllLeader": [
+//           "GreeceGorgo"
+//         ],
+//         "id": 9,
+//         "highestCareerSkill": 1760,
+//         "careerSeasonWins": 260,
+//         "careerSeasonLosses": 62,
+//         "careerSeasonPercent": 80,
+//         "teamSkill": 1890,
+//         "highestTeamSkill": 1500,
+//         "teamWins": 65,
+//         "teamLosses": 44,
+//         "teamPercent": 60,
+//         "teamBestLeader": "ChinaQinShiHuang",
+//         "teamAllLeader": [
+//           "GreeceGorgo"
+//         ],
+//         "teamSeasonWins": 203,
+//         "teamSeasonLosses": 28,
+//         "teamSeasonPercent": 88,
+//         "duelSkill": 1580,
+//         "highestDuelSkill": 1500,
+//         "duelWins": 10,
+//         "duelLosses": 21,
+//         "duelPercent": 32,
+//         "duelBestLeader": "KongoMvembaANzinga",
+//         "duelAllLeader": [
+//           "GreeceGorgo"
+//         ],
+//         "duelSeasonWins": 10,
+//         "duelSeasonLosses": 32,
+//         "duelSeasonPercent": 24,
+//         "ffaSkill": 1550,
+//         "highestffaSkill": 1500,
+//         "ffaWins": 70,
+//         "ffaLosses": 59,
+//         "ffaPercent": 54,
+//         "ffaBestLeader": "MacedonAlexander",
+//         "ffaAllLeader": [
+//           "GreeceGorgo"
+//         ],
+//         "ffaSeasonWins": 43,
+//         "ffaSeasonLosses": 26,
+//         "ffaSeasonPercent": 62,
+//         "careerHighSkill": 1787,
+//         "teamHighSkill": 1890,
+//         "duelHighSkill": 1580,
+//         "ffaHighSkill": 1780
+//       },
+//       false
+//     ]
+//   ]
