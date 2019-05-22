7d4cdbc43bd8659e14bc31f7e7db9f410e8480e1
BrianC <seahorse8789@gmail.com>
2019-05-22 13:23:50 -0700

    fixed bugs relating to add comments.

--- src/components/ForumTopics/index.js
+++ src/components/ForumTopics/index.js
@@ -2,16 +2,17 @@ import { connect } from 'react-redux';
 import ForumTopics from './ForumTopics';
 function mapStoreToProps(store){
     return {
-       viewingThread : store.ForumTopics.viewingThread,
-       newTopicActive: store.ForumMain  .newTopicActive,
-       blogs         : store.ForumTopics.blogs,
-       threads       : store.ForumTopics.threads,
-       id            : store.ForumTopics.id,
-       number        : store.ForumTopics.number,
-       signedIn      : store.ForumMain  .signedIn,
-       comment       : store.ForumTopics.comment,
-       count         : store.ForumTopics.count,
-       sortOrder     : store.Leaderboard.sortOrder,
+       viewingThread  : store.ForumTopics.viewingThread,
+       newTopicActive : store.ForumMain  .newTopicActive,
+       blogs          : store.ForumTopics.blogs,
+       threads        : store.ForumTopics.threads,
+       id             : store.ForumTopics.id,
+       number         : store.ForumTopics.number,
+       signedIn       : store.ForumMain  .signedIn,
+       comment        : store.ForumTopics.comment,
+       count          : store.ForumTopics.count,
+       sortOrder      : store.Leaderboard.sortOrder,
+       viewingThreadId: store.ForumTopics.viewingThreadId
     };
 }
 
